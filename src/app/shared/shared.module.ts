a456e3cc4d6ed2d479f9fcff45dd5fab11f36796
Lorenzo <lorenzorepetto28@gmail.com>
2019-11-23 21:02:03 -0300

    Primera MainNav

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,23 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 // Componentes
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 
 // Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    
+    MainNavComponent
   ],
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule
   ],
   exports:[
-    
+    MainNavComponent
   ]
 })
 export class SharedModule { }
