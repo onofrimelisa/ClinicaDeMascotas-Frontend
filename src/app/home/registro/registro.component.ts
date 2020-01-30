56eb7d081d8bde0f66fbdd25bbde6f69557c7563
Lorenzo <lorenzorepetto28@gmail.com>
2020-01-30 19:21:39 -0300

    Arreglos en Registro Usuario

--- src/app/home/registro/registro.component.ts
+++ src/app/home/registro/registro.component.ts
@@ -7,7 +7,7 @@ import { IUsuarioNuevo } from '../../interfaces/IUsuario';
 
 // SweetAlert
 import Swal from 'sweetalert2'
-import { UsuarioService, CargaImagenService } from '../../services/service.index';
+import { UsuarioService } from '../../services/service.index';
 
 
 @Component({
@@ -28,51 +28,41 @@ export class RegistroComponent implements OnInit {
   fecha_nacimiento: Date;
   telefono: string;
   rol: string;
-
-  // Foto
-  archivo: IFoto = null;
-  imagenTemp: string;
-  formFoto: FormGroup;
-  foto: string;
+  
 
   // Veterinario
   datosProfesionales: FormGroup;
   nombre_consultorio: string;
   domicilio_consultorio: string;
   matricula: string;
+
+  // Foto
+  // archivo: IFoto = null;
+  // imagenTemp: string;
+  // formFoto: FormGroup;
+  // foto: string;
+  // Foto 
+    // this.formFoto = this._formBuilder.group({
+    //   foto: ['']
+    // });
   
   constructor( private _formBuilder: FormBuilder,
                private usuarioService: UsuarioService,
-               private datePipe: DatePipe ) { 
-                 
-    // DatosPersonales STEP 1
-    this.datosPersonales = this._formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      nombre: ['', Validators.required],
-      apellido: ['', Validators.required],
-      fecha_nacimiento: ['', Validators.required],
-      telefono: ['', Validators.required],
-      rol: ['', Validators.required],
-      
-    });
-    
-    // Foto STEP 2
-    this.formFoto = this._formBuilder.group({
-      foto: ['']
-    });
-
-    // DatosProfesionales STEP 3
-    this.datosProfesionales = this._formBuilder.group({
-      nombre_consultorio: [''],
-      domicilio_consultorio: [''],
-      matricula: ['']
-    });
-  }
+               private datePipe: DatePipe ) { }
 
   ngOnInit() {
+    // DatosPersonales STEP 1
+    this.inicializarPersonales();
+    // DatosProfesionales STEP 3
+    this.inicializarProfesionales();
   }
   
+
+
+
+  //===============================================
+  //               REGISTRO
+  //===============================================
   registrarUsuario(usuarioNuevo: IUsuarioNuevo) {
     this.usuarioService.agregarUsuario(usuarioNuevo)
             .subscribe( (usuario) => {
@@ -113,6 +103,7 @@ export class RegistroComponent implements OnInit {
     }
     
     this.registrarUsuario(usuarioNuevo);
+
   }
 
 
@@ -120,34 +111,62 @@ export class RegistroComponent implements OnInit {
     this.eventoCambiar.emit(false);
   }
   
-  onFileSelected(archivo: File) {
-    
-    if ( archivo.type.indexOf('image') < 0 ) {
-      Swal.fire('Sólo imagenes', 'El archivo seleccionado no es una imagen', 'error');
-      this.archivo = null;
-      return;
-    }
-
-    let fullname = archivo['name'] 
-    let name = fullname.split('.')[0];
-    let ext = fullname.split('.')[1];
-
-    let reader = new FileReader();
-    let urlImagenTemp = reader.readAsDataURL( archivo );
-    reader.onloadend = () => this.imagenTemp = String(reader.result);
-    
-    this.archivo = {
-      archivo: archivo,
-      nombreArchivo: name,
-      extension: ext,
-      url: ''
-    }
-    console.log(this.archivo);
-  }
   
-  limpiarFoto(){
-    this.archivo = null;
-    this.imagenTemp = null;
+  getFecha() {
+    return this.datePipe.transform(this.datosPersonales.value.fecha_nacimiento, 'yyyy-MM-dd');
+  }
+
+  // onFileSelected(archivo: File) {
+    
+  //   if ( archivo.type.indexOf('image') < 0 ) {
+  //     Swal.fire('Sólo imagenes', 'El archivo seleccionado no es una imagen', 'error');
+  //     this.archivo = null;
+  //     return;
+  //   }
+
+  //   let fullname = archivo['name'] 
+  //   let name = fullname.split('.')[0];
+  //   let ext = fullname.split('.')[1];
+
+  //   let reader = new FileReader();
+  //   let urlImagenTemp = reader.readAsDataURL( archivo );
+  //   reader.onloadend = () => this.imagenTemp = String(reader.result);
+    
+  //   this.archivo = {
+  //     archivo: archivo,
+  //     nombreArchivo: name,
+  //     extension: ext,
+  //     url: ''
+  //   }
+  //   console.log(this.archivo);
+  // }
+  
+  // limpiarFoto(){
+  //   this.archivo = null;
+  //   this.imagenTemp = null;
+  // }
+
+  //===============================================
+  //                  FORMULARIOS
+  //===============================================
+  private inicializarPersonales() {
+    this.datosPersonales = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      fecha_nacimiento: ['', Validators.required],
+      telefono: ['', Validators.required],
+      rol: ['', Validators.required],
+    });
+  }
+
+  private inicializarProfesionales() {
+    this.datosProfesionales = this._formBuilder.group({
+      nombre_consultorio: [''],
+      domicilio_consultorio: [''],
+      matricula: ['']
+    });
   }
 
   setFormConfig(){
@@ -159,21 +178,12 @@ export class RegistroComponent implements OnInit {
       this.datosProfesionales.controls['domicilio_consultorio'].enable();
       this.datosProfesionales.controls['matricula'].enable();
     }
-    else {
-      this.datosProfesionales.controls['nombre_consultorio'].clearValidators();
-      this.datosProfesionales.controls['domicilio_consultorio'].clearValidators();
-      this.datosProfesionales.controls['matricula'].clearValidators();
-      this.datosProfesionales.controls['nombre_consultorio'].disable();
-      this.datosProfesionales.controls['domicilio_consultorio'].disable();
-      this.datosProfesionales.controls['matricula'].disable();
-    }
     this.datosProfesionales.controls['matricula'].updateValueAndValidity();
     this.datosProfesionales.controls['nombre_consultorio'].updateValueAndValidity();
     this.datosProfesionales.controls['domicilio_consultorio'].updateValueAndValidity();
   }
 
 
-  getFecha() {
-    return this.datePipe.transform(this.datosPersonales.value.fecha_nacimiento, 'yyyy-MM-dd');
-  }
+
+
 }
