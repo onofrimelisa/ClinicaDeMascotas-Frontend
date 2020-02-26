99276701107b66996de1b0988ae6ced8d3a5ec1c
onofrimelisa <mel.onofri@gmail.com>
2020-02-25 23:42:02 -0300

    Arreglo en el loading

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styles: [`
     mat-icon{
-      font-size: 50px;
+      font-size: 40px;
     }
     `
   ]
 })
 export class LoadingComponent implements OnInit {
+
+  @Input() mensaje: string = 'Cargando...'
 
   constructor() { }
 
